53384441d0a3708effb3ef9a91bf144e6a19b6b1
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-09-12 16:09:36 +0200

    Created main.cpp

---
+++ engine/main.cpp
@@ -0,0 +1,11 @@
+#include "ais/ais.cpp"
+#include "player/player.cpp"
+#include "rendering/rendering.cpp"
+#include "server/server.cpp"
+#include "teams/teams.cpp"
+
+namespace main() {
+    
+    
+    
+}
