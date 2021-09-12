dfe016168d5b8e19023f0e21c9edd12cb8518558
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-09-12 17:09:57 +0200

    Updated graphics.cpp

--- engine/graphics/graphics.cpp
+++ engine/graphics/graphics.cpp
@@ -1,19 +1,36 @@
+#include <list>
+
 namespace graphics() {
     
     int light() {
         
+        list<int> lights;
+        void createLight(xPos, yPos, zPos, xRotation, yRotation, zRotation, lightBrightness) {
+            
+            lights.push_back("{'xPos': " +xPos +", 'yPos': " +yPos +", 'zPos': " +zPos +", 'xRotation': " +xRotation +", 'yRotation': " +yRotation +", 'zRotation': " +zRotation +", 'lightBrightness': " +lightBrightness +"}")
+            
+        };
         
-        
-    }
+    };
     int shaders() {
         
+        list<int> shaders;
+        void createShader(xSize, ySize, shader) {
+            
+            shaders.push_back("{'xSize': " +xSize +", 'ySize': " +ySize +", 'texture': '" +shader +'"}")
+            
+        };
         
-        
-    }
+    };
     int texture() {
         
+        list<int> textures;
+        void createTexture(xSize, ySize, texture) {
+            
+            textures.push_back("{'xSize': " +xSize +", 'ySize': " +ySize +", 'texture': '" +texture +"'}")
+            
+        };
         
-        
-    }
+    };
     
 }
