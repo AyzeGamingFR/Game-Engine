02eed1f946a4c9db6b45b427978c08e469f87913
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-11-09 16:23:43 +0100

    Created movements.cpp

---
+++ engine/movements.cpp
@@ -0,0 +1,14 @@
+namespace movements {
+    
+    unsigned int jump(unsigned int distance) {
+        
+        
+        
+    };
+    unsigned int speed(unsigned int distance, unsigned int time) {
+        
+        return (distance / time)
+        
+    };
+    
+}
